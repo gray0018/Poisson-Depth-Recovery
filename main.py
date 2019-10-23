32d0696b01a4a011f531dd7408c19c1795a60109
yang.zhuoyu <yang.zhuoyu@ist.osaka-u.ac.jp>
2019-10-23 17:07:09 +0900

    change for debug

--- main.py
+++ main.py
@@ -76,14 +76,14 @@ class PoissonOperator(object):
 
 
         if mask_used[1] == 1: # left
-            D_ct = (data_used[0]+data_used[1])[0]/2
+            D_ct = -(data_used[0]+data_used[1])[0]/2
             colidx.append(position_used[1])
-            colvals.append(-1)
+            colvals.append(1)
             bvals += D_ct
         if mask_used[2] == 1: # down
-            D_cl = (data_used[0]+data_used[2])[1]/2
+            D_cl = -(data_used[0]+data_used[2])[1]/2
             colidx.append(position_used[2])
-            colvals.append(-1)
+            colvals.append(1)
             bvals += D_cl
         if mask_used[3] == 1: # up
             D_cr = (data_used[0]+data_used[3])[1]/2
@@ -145,11 +145,6 @@ class PoissonOperator(object):
         return self.depth
 
 
-def usage():
-    print("Orthographic example: python main.py -o normal.npy mask_normal.npy depth.npy mask_depth.npy camera.ini")
-    print("Projective example: python main.py -o normal.npy mask_normal.npy depth.npy mask_depth.npy camera.ini")
-
-
 def plot_est_result(depth_map):
     plt.figure()
     plt.imshow(depth_map, "gray")
@@ -159,49 +154,48 @@ def plot_est_result(depth_map):
     plt.show()
 
 
-if __name__ == '__main__':
 
-    dir = sys.argv[1]
+dir = "data/sphere-256"
 
-    n = np.load(dir+"/normal.npy")
-    n_mask = np.load(dir+"/normal_mask.npy")
-    n[..., 2][~n_mask] = -1
+n = np.load(dir+"/normal.npy")
+n_mask = np.load(dir+"/normal_mask.npy")
+n[..., 2][~n_mask] = -1
 
-    d = np.load(dir+"/depth.npy")
-    d_mask = np.load(dir+"/depth_mask.npy")
+d = np.load(dir+"/depth.npy")
+d_mask = np.load(dir+"/depth_mask.npy")
 
-    camera = np.loadtxt(dir+"/camera.ini")
+camera = np.loadtxt(dir+"/camera.ini")
 
-    if camera[2, 2] == 0:
-        # orthographic camera
-        Zc = camera[2, 3]
-        p = -n[..., 0] / n[..., 2] / Zc
-        q = -n[..., 1] / n[..., 2] / Zc
-        d[~d_mask] = 0
+if camera[2, 2] == 0:
+    # orthographic camera
+    Zc = camera[2, 3]
+    p = -n[..., 0] / n[..., 2] / Zc
+    q = -n[..., 1] / n[..., 2] / Zc
+    d[~d_mask] = 0
 
-        batch = PoissonOperator(np.dstack([p, q]), n_mask.astype(np.int8), d, 0.1)
-        d_est = batch.run()
+    batch = PoissonOperator(np.dstack([p, q]), n_mask.astype(np.int8), d, 0.1)
+    d_est = batch.run()
 
-        d_est[~n_mask] = np.nan
-        plot_est_result(d_est)
-    else:
-        # perspective camera
-        fx = camera[0, 0]
-        fy = camera[1, 1]
-        px = camera[0, 2]
-        py = camera[1, 2]
+    d_est[~n_mask] = np.nan
+    plot_est_result(d_est)
+else:
+    # perspective camera
+    fx = camera[0, 0]
+    fy = camera[1, 1]
+    px = camera[0, 2]
+    py = camera[1, 2]
 
-        x, y = np.meshgrid(np.arange(d.shape[1]), np.arange(d.shape[0]))
-        u = x - px
-        v = np.flipud(y) - py
+    x, y = np.meshgrid(np.arange(d.shape[1]), np.arange(d.shape[0]))
+    u = x - px
+    v = np.flipud(y) - py
 
-        p = -n[..., 0] / (u * n[..., 0] + v * n[..., 1] + fx * n[..., 2])
-        q = -n[..., 1] / (u * n[..., 0] + v * n[..., 1] + fy * n[..., 2])
-        d[~d_mask] = 1
-        d = np.log(d)
+    p = -n[..., 0] / (u * n[..., 0] + v * n[..., 1] + fx * n[..., 2])
+    q = -n[..., 1] / (u * n[..., 0] + v * n[..., 1] + fy * n[..., 2])
+    d[~d_mask] = 1
+    d = np.log(d)
 
-        batch = PoissonOperator(np.dstack([p, q]), n_mask.astype(np.int8), d, 0.1)
-        d_est = np.exp(batch.run())
+    batch = PoissonOperator(np.dstack([p, q]), n_mask.astype(np.int8), d, 0.1)
+    d_est = np.exp(batch.run())
 
-        d_est[~n_mask] = np.nan
-        plot_est_result(d_est)
+    d_est[~n_mask] = np.nan
+    plot_est_result(d_est)
